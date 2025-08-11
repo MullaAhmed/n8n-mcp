2d9dd0526f9c10339b0c24966c313e7443498b73
github-action-benchmark <github@users.noreply.github.com>
2025-08-11 03:21:58 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for 7eda4655920aa02223b36f0f39218e51af8f32bb

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754879982596,
+  "lastUpdate": 1754882518542,
   "repoUrl": "https://github.com/MullaAhmed/n8n-mcp",
   "entries": {
     "n8n-mcp Benchmarks": [
@@ -52,6 +52,58 @@ window.BENCHMARK_DATA = {
             "unit": "ms",
             "range": 0.2996,
             "extra": "14261 ops/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "67974888+MullaAhmed@users.noreply.github.com",
+            "name": "Ahmed Mulla",
+            "username": "MullaAhmed"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7eda4655920aa02223b36f0f39218e51af8f32bb",
+          "message": "Updated nodes.db-shm",
+          "timestamp": "2025-08-11T08:50:30+05:30",
+          "tree_id": "139145fb53e279c85d671871a117a0e755ea904b",
+          "url": "https://github.com/MullaAhmed/n8n-mcp/commit/7eda4655920aa02223b36f0f39218e51af8f32bb"
+        },
+        "date": 1754882518258,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0187,
+            "unit": "ms",
+            "range": 0.2383,
+            "extra": "53394 ops/sec"
+          },
+          {
+            "name": "sample - array sorting - large",
+            "value": 3.1398,
+            "unit": "ms",
+            "range": 0.4321999999999999,
+            "extra": "318 ops/sec"
+          },
+          {
+            "name": "sample - string concatenation",
+            "value": 0.0047,
+            "unit": "ms",
+            "range": 0.2542,
+            "extra": "214274 ops/sec"
+          },
+          {
+            "name": "sample - object creation",
+            "value": 0.066,
+            "unit": "ms",
+            "range": 0.29869999999999997,
+            "extra": "15157 ops/sec"
           }
         ]
       }
