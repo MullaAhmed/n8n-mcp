e5c0e84965423f739bcad64463ac1ab0c07a432a
github-action-benchmark <github@users.noreply.github.com>
2025-08-11 02:39:42 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for caa8e060096a8036c56f0562a7cd1b6975fafb28

---
+++ benchmarks/data.js
@@ -0,0 +1,60 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1754879982596,
+  "repoUrl": "https://github.com/MullaAhmed/n8n-mcp",
+  "entries": {
+    "n8n-mcp Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "67974888+MullaAhmed@users.noreply.github.com",
+            "name": "Ahmed Mulla",
+            "username": "MullaAhmed"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "caa8e060096a8036c56f0562a7cd1b6975fafb28",
+          "message": "Updated nodes.db",
+          "timestamp": "2025-08-11T08:08:00+05:30",
+          "tree_id": "0968239884784b8d2f4c601556a6521ce1929c9a",
+          "url": "https://github.com/MullaAhmed/n8n-mcp/commit/caa8e060096a8036c56f0562a7cd1b6975fafb28"
+        },
+        "date": 1754879982315,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0188,
+            "unit": "ms",
+            "range": 0.2733,
+            "extra": "53105 ops/sec"
+          },
+          {
+            "name": "sample - array sorting - large",
+            "value": 3.1287,
+            "unit": "ms",
+            "range": 0.5102000000000002,
+            "extra": "320 ops/sec"
+          },
+          {
+            "name": "sample - string concatenation",
+            "value": 0.0046,
+            "unit": "ms",
+            "range": 0.2518,
+            "extra": "217092 ops/sec"
+          },
+          {
+            "name": "sample - object creation",
+            "value": 0.0701,
+            "unit": "ms",
+            "range": 0.2996,
+            "extra": "14261 ops/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
