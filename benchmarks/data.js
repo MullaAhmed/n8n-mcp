1c08f73efc53fcabe147e1de0c5da26c38bfb372
github-action-benchmark <github@users.noreply.github.com>
2025-08-15 18:34:07 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for baab3a02dc68c5d81e33f93bce0e5462d65a52d1

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754883065718,
+  "lastUpdate": 1755282847932,
   "repoUrl": "https://github.com/MullaAhmed/n8n-mcp",
   "entries": {
     "n8n-mcp Benchmarks": [
@@ -260,6 +260,58 @@ window.BENCHMARK_DATA = {
             "unit": "ms",
             "range": 0.4901,
             "extra": "14818 ops/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "56956555+czlonkowski@users.noreply.github.com",
+            "name": "Romuald Członkowski",
+            "username": "czlonkowski"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "baab3a02dc68c5d81e33f93bce0e5462d65a52d1",
+          "message": "Merge pull request #139 from czlonkowski/feature/validation-improvements\n\nchore: update n8n to v1.106.3 and bump version to 2.10.4",
+          "timestamp": "2025-08-12T08:57:47+02:00",
+          "tree_id": "eb0e009eb0f4f9b2b57fab8bd3ab6b4442e72ccc",
+          "url": "https://github.com/MullaAhmed/n8n-mcp/commit/baab3a02dc68c5d81e33f93bce0e5462d65a52d1"
+        },
+        "date": 1755282847684,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0187,
+            "unit": "ms",
+            "range": 0.2484,
+            "extra": "53514 ops/sec"
+          },
+          {
+            "name": "sample - array sorting - large",
+            "value": 3.1466,
+            "unit": "ms",
+            "range": 0.5203000000000002,
+            "extra": "318 ops/sec"
+          },
+          {
+            "name": "sample - string concatenation",
+            "value": 0.0047,
+            "unit": "ms",
+            "range": 0.2757,
+            "extra": "210818 ops/sec"
+          },
+          {
+            "name": "sample - object creation",
+            "value": 0.0673,
+            "unit": "ms",
+            "range": 0.3541,
+            "extra": "14869 ops/sec"
           }
         ]
       }
